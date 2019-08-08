970e6b641e61e1d74c8ddcaecbe84bf82ec5701a
Arsen Yaremyn <arsenyaremin@gmail.com>
2019-08-08 21:52:35 +0300

    Added setters

--- src/com/company/homework_06/Employee.java
+++ src/com/company/homework_06/Employee.java
@@ -24,6 +24,18 @@ public class Employee {
         return salary;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
     @Override
     public String toString() {
         return "Employee{" +
